0e861d8f63e030499f26b4e45d23b68bcdaeb6d6
Minjae <minjae.jung0@gmail.com>
2020-07-30 21:15:55 -0400

    update

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors")
+
+const PORT = 5500;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(cors({
+  "origin": ["http://127.0.0.1:5500"],
+  "methods": ["GET", "POST", "PUT", "OPTIONS"]
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
